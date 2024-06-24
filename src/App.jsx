b26ad606e026351d3aed8132899728627b8b55db
minsung <menysung94@naver.com>
2024-06-24 14:01:05 +0900

    로컬에 저장하기

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ import doingIcon from "./assets/glowing-star.png";
 import TaskColumn from "./components/TaskColumn";
 import TaskForm from "./components/TaskForm";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+//처음 시작할 떄 저장된 문자열 데이터를 갖고온다
+const saveTasks = localStorage.getItem("tasks");
 //각각의 3개의 컬럼에 맞는 타입의 데이터 전달
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(JSON.parse(saveTasks) || []);
   const handleDelete = (taskIndex) => {
     const newTasks = tasks.filter((task, index) => index !== taskIndex);
     setTasks(newTasks);
   };
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   return (
     <div className="app">
       <TaskForm setTasks={setTasks} />
